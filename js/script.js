b1a64f27a498a8e1153d2729725bd27e37a73765
YuliyaAtrashkevich <trafimava@gmail.com>
2023-10-07 15:11:30 +0600

    Новый слайдер, черновой

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 // Анимация при скролле
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("activity");
+    } else {
+      reveals[i].classList.remove("activity");
     }
   }
-  window.addEventListener("scroll", reveal);
+}
+window.addEventListener("scroll", reveal);
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var modal = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(modal);
-    var zoom = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(zoom);
-    var carousel = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(carousel);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var modal = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(modal);
+  var zoom = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(zoom);
+  var carousel = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(carousel);
+});
