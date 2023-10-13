a506e520cb537e36e5809a9f0dc5d4538af5e527
YuliyaAtrashkevich <trafimava@gmail.com>
2023-10-13 20:15:34 +0600

    Финальные штрихи

---
+++ form.php
@@ -0,0 +1,7 @@
+<form class="modal__form" action="send.php" method="post">
+                <input class="modal__form-input" type="text" name="fio" placeholder="Ваше имя" required>
+                <input class="modal__form-input" type="email" name="email" placeholder="Ваш email" required>
+            </form>
+            <p class="uk-text-center">
+                <button class="uk-button uk-button-primary modal-close modal__footer-btn" type="submit" value="Отправить">Отправить</button>
+            </p>
